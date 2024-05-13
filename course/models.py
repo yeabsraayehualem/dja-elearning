2513c72b713b23e7a99f8d755f607505a38f61d3
yeabsraayehualem <yeabsraayehualem@gmail.com>
2024-05-13 22:02:17 +0300

    ui integration

--- course/models.py
+++ course/models.py
@@ -1,8 +1,25 @@
 from django.db import models
+
+from user.models import User
 class CourseCategory(models.Model):
     name = models.CharField(max_length=255, blank=False, null=False)
-    image = models.ImageField(upload_to="course-categories/images/", blank=False, null=False)
+    image = models.ImageField(upload_to="media/course-categories/images/", blank=False, null=False)
  
     
     def __str__(self):
         return self.name
+
+
+class Course(models.Model):
+    title= models.CharField(max_length=255,)
+    rating = models.CharField(max_length=20)
+    currently_enrolled = models.IntegerField()
+    price= models.IntegerField()
+    level =models.CharField(choices=[(i,i) for i in ['Beginner', "Intermediate",'Advanced']], max_length=120)
+    duration= models.DurationField()
+    category = models.ForeignKey(CourseCategory, on_delete=models.CASCADE)
+    image= models.ImageField( upload_to="media/course/images/",)
+    is_paid = models.BooleanField(default=False)
+    instructor = models.ForeignKey(User, on_delete=models.CASCADE, blank=True,null=True)
+    def __str__(self):
+        return f"{self.title}"
--- home/views.py
+++ home/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 
+from course.models import Course, CourseCategory
 from home.forms import UserForm
 
 # Create your views here.
 def home(req):
-    return render(req,'index.html')
+    return render(req,'index.html',{'courseCategory':CourseCategory.objects.all(),'course': Course.objects.all()})
 
 def about(req):
     return render(req,'about.html')
 
 def courses(req):
-    return render(req,'courses.html')
+    return render(req,'courses.html',{'courseCategory':CourseCategory.objects.all(),'course': Course.objects.all()})
 
 def contact(req):
     return render(req,'contact.html')
